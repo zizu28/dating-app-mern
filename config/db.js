952dda89cab43cedbf3d580ebf3a8fc2b74e994d
Zulka <ziblimzulka.zz@gmail.com>
2024-09-20 14:50:59 +0700

    Parsed environment var to string

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI.toString());
+        const conn = await mongoose.connect((process.env.MONGO_URI).toString());
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
