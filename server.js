156f3d61ecb1e7f7601164b389b028f443910786
Zulka <ziblimzulka.zz@gmail.com>
2024-09-20 15:11:31 +0700

    CORS issue

--- server.js
+++ server.js
@@ -10,14 +10,17 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    next()
-})
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+//     next()
+// })
 
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 app.use(express.json())
 app.use('/api/dating', cardRouter)
 app.use(cookieParser())
