6430266ebbe8162b5f222444e299d892466d9180
Zulka <ziblimzulka.zz@gmail.com>
2024-09-20 14:54:03 +0700

    Parsed environment var to string

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect((process.env.MONGO_URI).toString());
+        const conn = await mongoose.connect(
+            "mongodb+srv://ziblimzulkazz:Zz0$Zz6$0638@dating-app-mern.uzqwa.mongodb.net/?retryWrites=true&w=majority&appName=Dating-App-Mern"
+        );
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
